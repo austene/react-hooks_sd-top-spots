5c6f87e632ce3b0a5157547f2f9f7f87fa1f76ac
AE <austence@sbcglobal.net>
2021-09-17 10:57:53 -0700

    adds setup files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>San Diego Top Spots</header>
+      <p>info here</p>
+      <hr />
+
+      
+    </div>
+  );
+}
+
+export default App;
