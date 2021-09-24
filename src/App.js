42ae3b0f06161b588a4b3a758da0aa0e7b3fb2ef
AE <austence@sbcglobal.net>
2021-09-24 11:06:00 -0700

    adds useEffect axios call to topspots api. adds try and catch error handling in useEffect.  Adds handling displays for both API success and API failure.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  //states
+  const [hotspots, setHotspots] = React.useState({ hits: [], errorMessage: '' });
+
+  useEffect(() => {
+    async function fetchData() {
+      // Await response
+      try {
+        const response = await axios('https://origin-top-spots-api.herokuapp.com/api/topspots');
+        //set data to state
+        setHotspots({hits: response.data, errorMessage: ''})
+      }
+      //error
+      catch(error) {
+        setHotspots({hits: [], errorMessage: error.message})
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header>San Diego Top Spots</header>
       <p>info here</p>
       <hr />
+
+    {/* Display hotspots hits (success) */}
+    { !hotspots.errorMessage &&
+    <ul>
+      {hotspots.hits.map(item => (
+        <li key={item.id}>
+          {item.name}
+        </li>
+      ))}
+    </ul>
+    }
+
+    {/* Display error message (failure) */}
+    { hotspots.errorMessage && 
+    <h3 className='error'>{ hotspots.errorMessage }</h3>
+    }
+    <p>...whoops</p>
 
       
     </div>
